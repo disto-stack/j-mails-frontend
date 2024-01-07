d7b6b32065a7a3a2a62258f65ac3ec740914da75
disto-stack <juan.ocampo.2501@gmail.com>
2024-01-06 19:20:50 -0500

    feat(app): improve ui/ux

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -4,18 +4,21 @@ import ResultsTable from './ResultsTable.vue';
 import { Ref, ref } from 'vue';
 import { EmailSearchService } from '../services/email-search';
 import { Email } from '../types/email';
+import { MagnifyingGlassIcon } from '@heroicons/vue/24/solid';
 
 const searchInputText = ref('');
 const emails: Ref<Email[]> = ref([]);
 const selectedEmail: Ref<Email | null> = ref(null);
-
-const actualPage = ref(1);
 
 async function searchEmails() {
 	emails.value = [];
 	selectedEmail.value = null;
 
 	const value = searchInputText.value;
+
+	if (value.length === 0) {
+		return;
+	}
 
 	try {
 		const foundedElements = await EmailSearchService.searchByTerm(value);
@@ -37,26 +40,42 @@ function updateSelectedEmail(email: Email) {
 </script>
 
 <template>
-	<div class="min-h-screen flex flex-col justify-start">
-		<header class="w-full p-2 h-[10vh]">
-			<div>
+	<div
+		class="grid grid-cols-4 grid-rows-6 h-screen bg-gray-900 text-gray-200 p-8 gap-5"
+	>
+		<header class="flex justify-between col-span-2">
+			<h1
+				class="text-3xl font-bold text-gray-100 bg-clip-text text-transparent bg-gradient-to-r from-purple-400 via-pink-500 to-red-500"
+			>
+				Jmails
+			</h1>
+			<div class="relative">
 				<input
-					class="border shadow-md rounded-2xl border-gray-100 w-full p-2 focus:outline-none"
+					class="border-gray-100 focus:outline-none border-0 bg-gray-800 text-gray-200 p-2 w-64 rounded-full pl-10"
 					type="search"
 					name="search"
 					v-model="searchInputText"
 					@keyup.enter="searchEmails"
 					placeholder="Search email"
 				/>
+
+				<button @click="searchEmails">
+					<MagnifyingGlassIcon
+						class="w-6 h-6 text-gray-400 absolute top-2 left-2"
+					/>
+				</button>
 			</div>
 		</header>
 
-		<main class="w-full lg:flex h-[88vh]">
+		<main class="p-2 col-span-2 row-start-2 row-end-7">
 			<ResultsTable
 				:founded-emails="emails"
 				@select-email="updateSelectedEmail"
 			></ResultsTable>
-			<EmailContent :content="selectedEmail?.content"></EmailContent>
 		</main>
+
+		<aside class="col-span-2 row-start-1 row-end-7">
+			<EmailContent :content="selectedEmail?.content"></EmailContent>
+		</aside>
 	</div>
 </template>
--- src/components/EmailContent.vue
+++ src/components/EmailContent.vue
@@ -7,10 +7,8 @@ defineProps<EmailContentProps>();
 </script>
 
 <template>
-	<div
-		class="shadow-md rounded-2xl lg:w-2/3 border-gray-100 m-2 p-4 block overflow-y-auto"
-	>
+	<section class="p-8 bg-gray-800 rounded-xl h-full overflow-y-auto">
 		<span v-if="!content">No e-mails selected</span>
 		<p v-if="content" class="whitespace-pre-wrap">{{ content }}</p>
-	</div>
+	</section>
 </template>
